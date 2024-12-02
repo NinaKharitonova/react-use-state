cbbcfd4eb08ade8c50e60f16cb33e3ac149add76
NinaKharitonova <ninakharitonova95@email.com>
2024-12-02 17:48:11 +0100

    Sistemazione cartelle

---
+++ src/button/Button.jsx
@@ -0,0 +1,16 @@
+import languages from "../languages";
+
+export default function button() {
+  return (
+    <>
+      <section className="conteiner-button">
+        <button className="btn">HTML</button>
+        <button className="btn">CSS</button>
+        <button className="btn">JavaScript</button>
+        <button className="btn">Node.js</button>
+        <button className="btn">Express</button>
+        <button className="btn">React.js</button>
+      </section>
+    </>
+  );
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,11 @@
+import Button from "../button/button";
+
 export default function Main() {
   return (
     <>
       <main>
         <section className="conteiner-button">
-          <button className="btn">HTML</button>
-          <button className="btn">CSS</button>
-          <button className="btn">JavaScript</button>
-          <button className="btn">Node.js</button>
-          <button className="btn">Express</button>
-          <button className="btn">React.js</button>
+          <Button />
         </section>
         <section>
           <div className="container-descrition">
